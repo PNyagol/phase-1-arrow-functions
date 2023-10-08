15b5668dbe4af79158054569e984ddf1ec31d967
Nyagol <nyagolpeter@gmail.com>
2023-10-08 06:36:01 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression called divide
+function divide() {
+    return 2000 / 100;
+  }
+  
+  // Arrow function called square
+  const square = (number) => {
+    return number * number;
+  };
+  
+  // Arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
